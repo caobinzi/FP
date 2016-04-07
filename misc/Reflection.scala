e0544b55d1487438ba3614a64711137cbd022249
Binzi Cao <caobinzi@gmail.com>
2016-04-07 10:21:25 +1000

    reflection test

---
+++ misc/Reflection.scala
@@ -0,0 +1,21 @@
+
+import scalaz._
+import scalaz.Scalaz._
+import scala.reflect.runtime.{universe => ru}
+//Some test about how to using reflection in scala
+object Reflect {
+  def getSymbols[T: ru.TypeTag]: List[ru.Symbol] = ru.typeOf[T].members.toList
+
+  def getSymbols(obj: Any): List[ru.Symbol] =
+  ru.runtimeMirror(getClass.getClassLoader).reflect(obj).symbol.toType.members.toList
+
+  def hasType[T2: ru.TypeTag](obj: Any, s: String): Boolean =
+  getSymbols(obj).exists{
+    x =>
+    (!x.isMethod) &&
+    (x.name.toString.trim === s) &&
+    x.typeSignature =:= ru.typeOf[T2]
+  }
+
+  def getFields(obj: Any): List[ru.Symbol] = getSymbols(obj).filter(!_.isMethod) 
+}
