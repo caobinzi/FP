5fc0d39a78d52d0d90f4d3f35bc89c62af53c0a3
Binzi Cao <caobinzi@gmail.com>
2016-09-09 09:57:00 +1000

    Some tests

--- Future/src/main/scala/App.scala
+++ Future/src/main/scala/App.scala
@@ -7,19 +7,6 @@ import scala.concurrent._
 import ExecutionContext.Implicits.global
 
 object FutureApp extends App {
-  val task1 = Future{
-    println("task1 start")
-    Thread.sleep(10000)
-    println("task1 end")
-    1
-  }
-
-  val task2 = Future{
-    println("task2 start")
-    Thread.sleep(10000)
-    println("task2 end")
-    2
-  }
   def task(s: Int) = Future{
     println(s"task ${s} start")
     Thread.sleep(10000)
@@ -27,18 +14,45 @@ object FutureApp extends App {
     s
   }
 
-  //  val task3 = for {
-  //    t1 <- task1
-  //    t2 <- task2
-  //  } yield {
-  //    t1 + t2
-  //    println("task3 Got here")
-  //  }
-  val task4 = List(10, 11, 12).map(task).sequenceU.foreach {
-    case x =>
-      println(x)
-      println("task4 Got here")
+  def task1 = Future{
+    println(s"task 1 start")
+    Thread.sleep(10000)
+    println(s"task 1 end")
+    1
   }
+  def task2 = Future{
+    println(s"task 2 start")
+    Thread.sleep(10000)
+    println(s"task 2 end")
+    2
+  }
+
+
+
+   // val task3 = for {
+   //   t1 <- task1
+   //   t2 <- task2
+   // } yield {
+   //   t1 + t2
+   //   println("task3 Got here")
+   // }
+
+  //val task4 = List(10, 11, 12).map(task).sequenceU.foreach {
+   // case x =>
+    //  println(x)
+     // println("task4 Got here")
+  //}
+
+   val task5 = for {
+     t1 <- Future{println("11111"); Thread.sleep(10000); 1}
+     t2 <- Future{println("22222"); Thread.sleep(10000); 100}
+   } yield {
+     t1 + t2
+     println("task3 Got here")
+   }
+
+
+
 
   Thread.sleep(1000000)
 
