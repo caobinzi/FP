34fa60a34886a13673ba8004a0de106cd7b6c4a8
Binzi Cao <binzi.cao@bt.com>
2017-11-04 19:00:45 +1100

    test

--- eff/EffBill/src/main/scala/Interact.scala
+++ eff/EffBill/src/main/scala/Interact.scala
@@ -12,40 +12,35 @@ sealed trait Interact[A]
 
 case class Ask(prompt: String) extends Interact[String]
 case class Tell(msg:   String) extends Interact[Unit]
+case object Stop extends Interact[Unit]
 
 object Interact {
   import org.atnos.eff._
   type _interact[R]    = Interact |= R
   type WriterString[A] = Writer[String, A]
   def myDate = new java.util.Date
-  def readLine(): String =
-    "snuggles"
-  def askUser[R: _interact](prompt: String): Eff[R, String] =
-    send(Ask(prompt))
-
-  def tellUser[R: _interact](message: String): Eff[R, Unit] =
-    send(Tell(message))
+  def readLine(): String = scala.io.StdIn.readLine()
 
   def runInteract[R, A](effect: Eff[R, A])(implicit m: Interact <= R): Eff[m.Out, A] =
     recurse(effect)(new Recurser[Interact, m.Out, A, A] {
       def onPure(a: A): A = a
       def ask(s:    String): String = {
-        println("Wait for 2 seconds")
+        println(s)
         Thread.sleep(2000)
         readLine()
       }
+      def tell(msg: String) = println(msg)
 
-      def onEffect[X](i: Interact[X]): X Either Eff[m.Out, A] = Left {
+      def onEffect[X](i: Interact[X]): X Either Eff[m.Out, A] =
         i match {
-          case Ask(prompt) => ask(prompt)
-          case Tell(msg) =>
-            println(msg)
+          case Ask(prompt) => Left(ask(prompt))
+          case Tell(msg)   => Left(tell(msg))
         }
-      }
+
       def onApplicative[X, T[_]: Traverse](ms: T[Interact[X]]): T[X] Either Interact[T[X]] =
         Left(ms.map {
           case Ask(prompt) => ask(prompt)
-          case Tell(msg)   => println(msg)
+          case Tell(msg)   => tell(msg)
         })
 
     })(m)
