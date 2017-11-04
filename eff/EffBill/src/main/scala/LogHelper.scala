5dbd24a54797c12ca8a61d17dddc0c4e0f195cda
Binzi Cao <binzi.cao@bt.com>
2017-11-04 22:11:14 +1100

    Log

--- eff/EffBill/src/main/scala/LogHelper.scala
+++ eff/EffBill/src/main/scala/LogHelper.scala
@@ -11,11 +11,12 @@ object LogHelper {
   implicit class LogTimesOps[R, A](e:    Eff[R, A]) {
     def logTimes[T[_]](implicit memberT: MemberInOut[T, R],
                        writer:           MemberIn[WriterString, R]): Eff[R, A] =
-      logTimes2[R, T, A](e)
+      LogHelper.logTimes[R, T, A](e)
   }
 
-  def logTimes2[R, T[_], A](eff:                    Eff[R, A])(implicit memberT: MemberInOut[T, R],
-                                            writer: MemberIn[WriterString, R]): Eff[R, A] = {
+  def logTimes[R, T[_], A](eff:                    Eff[R, A])(implicit memberT: MemberInOut[T, R],
+                                           writer: MemberIn[WriterString, R]): Eff[R, A] = {
+
     translateInto(eff)(new Translate[T, R] {
       def apply[X](tx: T[X]): Eff[R, X] =
         for {
