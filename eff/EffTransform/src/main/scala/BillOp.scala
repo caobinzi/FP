ec6e98c93654c4086c44211f83f18b51948f94e0
Binzi Cao <binzic@campainmonitor.com>
2018-02-14 23:06:32 +1100

    using natural transformation

---
+++ eff/EffTransform/src/main/scala/BillOp.scala
@@ -0,0 +1,44 @@
+import scala.language.higherKinds
+import scala.language.implicitConversions
+import cats._
+import data._
+import org.atnos.eff._
+import org.atnos.eff.all._
+import org.atnos.eff.syntax.all._
+import cats.implicits._
+import org.atnos.eff._, interpret._
+
+sealed trait BillOp[A]
+
+case class CheckBill(bill: String) extends BillOp[Boolean]
+
+case class UpdateBill(bill: String, status: String) extends BillOp[Option[String]]
+
+object BillOp {
+  import org.atnos.eff._
+
+  type _billOp[R] = BillOp |= R
+
+  val nt = new (BillOp ~> Eval) {
+    def apply[A](fa: BillOp[A]): Eval[A] =
+      fa match {
+        case UpdateBill(bill, card) => Now(Some("s"))
+        case CheckBill(bill)        => Now(true)
+      }
+  }
+  def runBillOp[R, BR, U, A](effect: Eff[R, A])(
+    implicit
+    sr: Member.Aux[BillOp, R, U],
+    br: Member.Aux[Eval, BR, U]
+  ): Eff[BR, A] = {
+    transform(effect, nt)
+  }
+
+  implicit class Bill[R, BR, U, A](effect: Eff[R, A]) {
+    def runBill(implicit
+      sr: Member.Aux[BillOp, R, U],
+                br: Member.Aux[Eval, BR, U]): Eff[BR, A] =
+      runBillOp[R, BR, U, A](effect)
+
+  }
+}
