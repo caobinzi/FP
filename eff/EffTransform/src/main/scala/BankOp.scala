14d5440b46449fcb27547874c341523bb2e17db3
Binzi Cao <binzic@campainmonitor.com>
2018-02-19 10:09:37 +1100

    Add bank translate code sample

--- eff/EffTransform/src/main/scala/BankOp.scala
+++ eff/EffTransform/src/main/scala/BankOp.scala
@@ -11,14 +11,14 @@ import org.atnos.eff._, interpret._
 sealed trait BankOp[A]
 
 case class Purchase(bill: String, card: String) extends BankOp[Option[String]]
-case class Refund(bill: String, card: String) extends BankOp[Option[String]]
+case class Refund(bill:   String, card: String) extends BankOp[Option[String]]
 
 object BankOp {
   import org.atnos.eff._
 
   type _bankOp[R] = BankOp |= R
   def purchase(bill: String, card: String) = "Ok".some
-  def check(bill: String) = bill === "1234"
+  def check(bill:    String) = bill === "1234"
   val nt = new (BankOp ~> Eval) {
     def apply[A](fa: BankOp[A]): Eval[A] =
       fa match {
