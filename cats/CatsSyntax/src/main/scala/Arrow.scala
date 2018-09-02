31e3cc9b71ae44efff6adbdfd296bb8cd6c30770
Binzi Cao <binzic@campainmonitor.com>
2018-09-02 20:30:33 +1000

    Clean

--- cats/CatsSyntax/src/main/scala/Arrow.scala
+++ cats/CatsSyntax/src/main/scala/Arrow.scala
@@ -3,20 +3,42 @@ import cats.implicits._
 object ArrowSyntax {
 
   def run = {
-    val toLong:   Int   => Long   = _.toLong
-    val toDouble: Float => Double = _.toDouble
-    val toS:      Int   => String = "s" * _
-    val ltoS:     Long  => String = x => s"s -> ${x}"
 
-    val f1: ((Int, Float)) => (Long, Double) = toLong *** toDouble
+    val toLong:   String => Long   = _.toLong
+    val toInt:    String => Int    = _.toInt
+    val toDouble: String => Double = _.toDouble
 
-    val f2: (Int) => (Long, String) = toLong &&& toS
+    val longToString: Long => String = x => s"s -> ${x}"
+    val intToString:  Int  => String = x => s"s -> ${x}"
 
-    val f3: Int => String = toLong >>> ltoS
+    /*
+     *Arrow
+     *  A---->B  C---->D ===> (A, C) => (B, D)
+     *  A---->B  A---->C ===> (A) => (B, C)
+     */
+    val f1: ((String, String)) => (Long, Double) = toLong *** toDouble
+    val f2: String             => (Long, Int)    = toLong &&& toInt
 
-    val f4: Int => String = ltoS <<< toLong
+    /*
+     *Compose
+     *  A---->B  C---->D ===> A => D
+     */
+    //Compose
+    val f3: String => String = toLong >>> longToString
+    val f4: String => String = longToString <<< toLong
 
-    val f5: Either[Int, Long] => Either[String, String] = toS +++ ltoS
-    val f6: Either[Int, Long] => String                 = toS ||| ltoS
+    /*
+     *Arrow Choice
+     *  A---->B  C---->D ===> Either[A, C] => Either[B, D]
+     */
+
+    val f5: Either[String, String] => Either[Int, Long] = toInt +++ toLong
+
+    /*
+     *Choice
+     *  A---->C  B---->C ===> Either[A, B] => C
+     */
+    val f6: Either[Int, Long] => String = intToString ||| longToString
+
   }
 }
